10795c6a232f5fb03d7c193140c2a8c58ca21630
a1ienist <aoooaaooo6@gmail.com>
2024-03-19 21:37:59 +0200

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { message } from 'telegraf/filters';
 import 'dotenv/config'
 import URL from "./index"
 
-const bot = new Telegraf('6652374350:AAGImEVGFx1EHJhoX02YB1BZn-YP6HvfrLk')
+const bot = new Telegraf(TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
